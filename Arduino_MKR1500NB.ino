b3ce93254c867ca2b96a3ed7b3e0ce9cb434746b
mdelain <mdelain@users.noreply.github.com>
2020-04-03 12:24:40 +0200

    Adding MQTTS support
    Now supports SSL connection to Live Objects (activated by default). For this, the sketch loads the DigiCert Root CA certificate in the radio module.
    Also more comments in the code ;)

--- Arduino_MKR1500NB.ino
+++ Arduino_MKR1500NB.ino
@@ -1,60 +1,107 @@
-#define DEFAULT_TX_FREQUENCY 60 // message rate by default
-                                // (can be updated over the air from Live Objects platform)
+/******************************************************************************
+   DEFAULT VALUES
+ ******************************************************************************/
+
+  // Set MQTT security:
+  // comment the line to disable security (MQTT),
+  // uncomment to activate TLS security (MQTTS).
+#define MQTT_TLS
+
+  // Set the default message transmission rate (in seconds).
+  // This parameter can be updated later over the air from Live Objects.
+#define DEFAULT_MESSAGE_RATE 60
+
+#define SW_REVISION "1.5.0"
+
+/******************************************************************************
+   INCLUDES
+ ******************************************************************************/
 
 #include "arduino_secrets.h"
+#if defined MQTT_TLS
+  #include "LiveObjectsCert.h"
+#endif
+#include <MKRNB.h>
 #include <ArduinoMqttClient.h>
 #include <ArduinoJson.h>
-#include <MKRNB.h>
 
-#define sw_revision "1.0.1"
+/******************************************************************************
+   TYPEDEFS
+ ******************************************************************************/
 
-NB nbAccess;
-//GPRS gprs;
-NBClient nbClient;
-MqttClient mqttClient(nbClient);
+enum NetworkConnectionAction {
+  CONNECT,
+  DISCONNECT
+};
 
-const char mqtt_user[] = "json+device";
-const char mqtt_pass[] = SECRET_LIVEOBJECTS_API_KEY;
+/******************************************************************************
+   CONSTANTS
+ ******************************************************************************/
+
+  // Live Objects information
 const char mqtt_id[] = "MKRNB1500";
 const char mqtt_broker[] = "liveobjects.orange-business.com";
-const char mqtt_pubdata[] = "dev/data";
-const char mqtt_pubcfg[] = "dev/cfg";
-const char mqtt_subcfg[] = "dev/cfg/upd";
+const char mqtt_user[] = "json+device";     // MQTT username for 'device' role
+#if defined MQTT_TLS
+  const uint16_t mqtt_port = 8883;          // TLS MQTT port
+#else
+  const uint16_t mqtt_port = 1883;          // plain MQTT port
+#endif
+const char mqtt_pubdata[] = "dev/data";     // topic for publishing data
+const char mqtt_pubcfg[] = "dev/cfg";       // subscribed topic for configuration updates
+const char mqtt_subcfg[] = "dev/cfg/upd";   // topic for publishing confuguration
 
-const char pin[] = SECRET_PINNUMBER;
-const char apn[] = SECRET_APN;
-const char apn_user[] = SECRET_APN_USER;
-const char apn_pass[] = SECRET_APN_PASS;
-
+  // Buffers holding the serialized default payloads for data and configuration
 const char* JSONdata = "{\"model\":\"github_sample_MKR\",\"value\":{\"uptime\":0}}";
-const char* JSONcfg= "{\"cfg\":{\"transmit frequency (s)\":{\"t\":\"u32\",\"v\":0}}}";
+const char* JSONcfg = "{\"cfg\":{\"message rate (s)\":{\"t\":\"u32\",\"v\":0}}}";
 
-uint32_t transmissionFrequency = DEFAULT_TX_FREQUENCY * 1000;
-uint32_t lastTransmission = DEFAULT_TX_FREQUENCY * 1000;
+/******************************************************************************
+   VARIABLES
+ ******************************************************************************/
 
-uint32_t uptimeInSec = 0;
+uint32_t messageRate = DEFAULT_MESSAGE_RATE * 1000;   // stores the current data message rate
+uint32_t lastMessageTime = DEFAULT_MESSAGE_RATE * 1000;   // stores the time when last data message was sent
+uint32_t uptimeInSec;   // stores the device uptime in seconds (sent in the data message)
+bool DigiCert_rootCA_loaded = false;  // controls if the root CA certificate has already been stored in the SARA module
 
+void connectionManager(NetworkConnectionAction _action);
+
+/******************************************************************************
+   OBJECTS CREATION
+ ******************************************************************************/
+
+NB nbAccess;
+#if defined MQTT_TLS
+  NBSSLClient nbClient;   // for TLS MQTT connection to Live Objects
+#else
+  NBClient nbClient;    // for plain MQTT connection to Live Objects
+#endif
+MqttClient mqttClient(nbClient);
 StaticJsonDocument<350> payload;
 
-void connectionManager(bool _way);
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
-  Serial.print("\n\n***\nUsing Arduino LTE-M with Live Objects\nversion ");
-  Serial.println(sw_revision);
-  Serial.println("***\n");
+  while (!Serial);  // uncomment this line if you want to run the sketch without monitoring the Serial line.
+  Serial.print("\n***\nLive Objects on Arduino MKR 1500NB\nrevision ");
+  Serial.println(SW_REVISION);
+  Serial.println("***");
+
+  if (sizeof(SECRET_LIVEOBJECTS_API_KEY) != 33) {
+    Serial.print("Please check your Live Objects API key (arduino_secrets.h file).\nStopping here.");
+    while (1) ;
+  }
 
   mqttClient.setId(mqtt_id);
-  mqttClient.setUsernamePassword(mqtt_user, mqtt_pass);
+  mqttClient.setUsernamePassword(mqtt_user, SECRET_LIVEOBJECTS_API_KEY);
   mqttClient.onMessage(onMessageReceived);
 
-  connectionManager(1);
+  connectionManager(CONNECT);
   updateConfig();
 }
 
 void loop() {
-  if (millis() - lastTransmission > transmissionFrequency) {
+  if (millis() - lastMessageTime > messageRate) {
     // get data from sensors
     Serial.println("Sampling data");
     sampleData();
@@ -63,7 +110,7 @@ void loop() {
     Serial.println("Sending data to Live Objects");
     //if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY || !mqttClient.connected())
     if (nbAccess.status() != NB_READY || !mqttClient.connected())
-      connectionManager(1);
+      connectionManager(CONNECT);
 
     sendData();
   }
@@ -72,23 +119,38 @@ void loop() {
     mqttClient.poll();
 }
 
-void connectionManager(bool _way = 1) {
-  switch (_way) {
-    case 1:
+void connectionManager(NetworkConnectionAction _action = CONNECT) {
+  switch (_action) {
+    case CONNECT:
       Serial.print("Connecting to cellular network");
-      //while ((nbAccess.begin() != NB_READY) || (gprs.attachGPRS() != GPRS_READY))
-      while (nbAccess.begin(pin, apn, apn_user, apn_pass) != NB_READY)
+      while (nbAccess.begin(SECRET_PINNUMBER, SECRET_APN, SECRET_APN_USER, SECRET_APN_PASS) != NB_READY)
       	Serial.print(".");
-      
       Serial.println("\nYou're connected to the network");
-      
+
+      #if defined MQTT_TLS    // only performed when using TLS, in order to load the needed root CA into the SARA module
+        if (!DigiCert_rootCA_loaded) {
+          Serial.println("Loading DigiCert Root CA certificate");
+          MODEM.sendf("AT+USECMNG=0,0,\"%s\",%d", LO_ROOT_CERT.name, LO_ROOT_CERT.size);
+          if (MODEM.waitForPrompt() != 1) {
+            Serial.print("Problem loading certificate!\nStopping here.");
+            while (1) ;
+          }
+          else {
+            MODEM.write(LO_ROOT_CERT.data, LO_ROOT_CERT.size);
+            int ready;
+            while (!MODEM.ready()) ;
+            DigiCert_rootCA_loaded = true;
+            Serial.println("Certificate loaded");
+          }
+        }
+      #endif
+
       Serial.print("Connecting to MQTT broker '");
       Serial.print(mqtt_broker);
       Serial.print("'");
       
-      while (!mqttClient.connect(mqtt_broker))
+      while (!mqttClient.connect(mqtt_broker, mqtt_port))
         Serial.print(".");
-      
       Serial.println("\nYou're connected to the MQTT broker");
       Serial.println();
 
@@ -97,7 +159,7 @@ void connectionManager(bool _way = 1) {
 
       break;
 
-    case 0:
+    case DISCONNECT:
       Serial.println("\nClosing MQTT connection...");
       mqttClient.stop();
       Serial.println("Disconnecting from cellular network...");
@@ -144,14 +206,14 @@ void onMessageReceived(int messageSize) {
 void updateConfig() {
   if (payload.containsKey(F("cid"))) {
     if (payload[F("cfg")].containsKey(F("transmit frequency (s)"))) {
-      transmissionFrequency = payload[F("cfg")][F("transmit frequency (s)")][F("v")].as<int>() * 1000;
-      payload[F("cfg")][F("transmit frequency (s)")][F("v")] = transmissionFrequency / 1000;
+      messageRate = payload[F("cfg")][F("transmit frequency (s)")][F("v")].as<int>() * 1000;
+      payload[F("cfg")][F("transmit frequency (s)")][F("v")] = messageRate / 1000;
     }
   }
   else {
     payload.clear();
     deserializeJson(payload, JSONcfg);
-    payload[F("cfg")][F("transmit frequency (s)")][F("v")] = transmissionFrequency / 1000;
+    payload[F("cfg")][F("transmit frequency (s)")][F("v")] = messageRate / 1000;
   }
 
   char _buffer[300];
@@ -172,5 +234,5 @@ void sendData() {
   char _buffer[300];
   serializeJson(payload, _buffer);
   publishMessage(mqtt_pubdata, _buffer);
-  lastTransmission = millis();
+  lastMessageTime = millis();
 }
--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,12 +1,12 @@
-//// cellular connection cerdentials
-#define SECRET_PINNUMBER ""		// unless PIN is deactivated, specify your SIM card PIN number
-#define SECRET_APN ""					// specify the APN name (if needed)
-#define SECRET_APN_USER "" 		// specify the username for your APN (if needed)
-#define SECRET_APN_PASS ""		// specify the password for your APN (if needed)
+	// Cellular connection cerdentials
+const char SECRET_PINNUMBER[] = "";		// unless PIN is deactivated, specify your SIM card PIN number
+const char SECRET_APN[] = "";					// specify the APN name (if needed)
+const char SECRET_APN_USER[] = ""; 		// specify the username for your APN (if needed)
+const char SECRET_APN_PASS[] = "";		// specify the password for your APN (if needed)
 
 
-//// your Live Objects credential
-#define SECRET_LIVEOBJECTS_API_KEY "..."	// paste here the Live Objects API key for your device;
-						// your key must use the "MQTT device" profile
-																					// You API key must have the predefined 'MQTT Device' right (alternatively: 'Device Access' read + write rights).
-																					// If your API key has more important rights, you *need* to use a TLS connection (MQTTS).
+	// Live Objects credential: paste below your API key (see Configuration > API keys on the portal).
+	// You API key must have at least the predefined 'MQTT Device' rights profile
+	// (alternatively: 'Device Access' read + write rights if need to customise the rights).
+	// Please note that you *must* use a TLS connection (MQTTS) if you grant more rights to the API key.
+const char SECRET_LIVEOBJECTS_API_KEY[] = "...";
